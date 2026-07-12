06fab1581a0044cfe1fd2ee80d1a4256934d69b8
Bryan Santiago Muñoz Romero <tiagotroller021019@gmail.com>
2026-07-12 16:15:45 -0500

    Mensaje del commit

--- src/middlewares/tenantMiddleware.js
+++ src/middlewares/tenantMiddleware.js
@@ -3,6 +3,9 @@ const { ForbiddenError } = require('../exceptions');
 function tenantMiddleware(req, res, next) {
   const artistId = req.user && req.user.artistId;
   if (!artistId) {
+    if (req.user && req.user.role === 'superadmin' && req.artistId) {
+      return next();
+    }
     return next(new ForbiddenError('No artist context available'));
   }
   req.artistId = artistId;
